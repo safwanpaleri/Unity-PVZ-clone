40a4975febc84a7a9186a090f436be76b6ddc783
safwanpaleri <safwanpaleri@gmail.com>
2021-11-10 17:40:17 +0530

    Add files via upload

---
+++ AttackerScript.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AttackerScript : MonoBehaviour
+{
+    GameObject currenttarget;
+    //[Range(0f, 3f)] [SerializeField] float speed = 1f;
+    float currentspeed = 1f;
+
+    private void Awake()
+    {
+        FindObjectOfType<LevelController>().IncreaseAttackers();
+    }
+    private void OnDestroy()
+    {
+        FindObjectOfType<LevelController>().DecreaseNoOfAttackers();
+    }
+
+   
+    void Update()
+    {
+       transform.Translate(Vector2.left * currentspeed * Time.deltaTime);
+        updateAnimation();
+    }
+
+    private void updateAnimation()
+    {
+        if(!currenttarget)
+        {
+            GetComponent<Animator>().SetBool("isattacking", false);
+        }    
+    }
+
+    public void SetMovementSpeed(float speed)
+    {
+        currentspeed = speed;
+    }
+
+    public void attackmode(GameObject otherobject)
+    {
+        GetComponent<Animator>().SetBool("isattacking", true);
+        currenttarget = otherobject;
+    }
+
+    public void strikedefender(float damage)
+    {
+        if (!currenttarget) { return; }
+        HealthScript health = currenttarget.GetComponent<HealthScript>();
+        if (health)
+        {
+            health.DealDamage(damage);
+        }
+}
+}
---
+++ AttackerSpwanerScript.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AttackerSpwanerScript : MonoBehaviour
+{
+    [Header("SPAWNING")]
+    bool spwan = true;
+    [SerializeField] float minDelay = 1f;
+    [SerializeField] float maxDelay = 5f;
+
+    [Header("Attacker")]
+    [SerializeField] AttackerScript[] LizardAttacker;
+    // Start is called before the first frame update
+    IEnumerator Start()
+    {
+        if (spwan)
+        {
+            yield return new WaitForSeconds(Random.Range(minDelay, maxDelay));
+            spawnAttacker();
+        }
+    }
+
+    public void StopSpawn()
+    {
+        spwan = false;
+       
+    }
+    private void spawnAttacker()
+    {
+            int index = Random.Range(0, LizardAttacker.Length);
+            spawnindex(LizardAttacker[index]);
+    }
+
+    private void spawnindex(AttackerScript newAttacker)
+    {
+        AttackerScript newAttcker = Instantiate (newAttacker, transform.position, Quaternion.identity) as AttackerScript;
+        newAttcker.transform.parent = transform;
+    }
+
+    public void updatedelay(float diff)
+    {
+        if(diff == 0 )
+        {
+            minDelay = 8f;
+            maxDelay = 12f;
+        }
+        else if(diff == 1)
+        {
+            minDelay = 5f;
+            maxDelay = 8f;
+        }
+        else if(diff == 2)
+        {
+            minDelay = 3f;
+            maxDelay = 5f;
+        }
+        else if (diff ==3)
+        {
+            minDelay = 1f;
+            maxDelay = 2f;
+        }
+    }    
+
+}
