2ffc9b054c841365cb47e5c3dce53a2214d4e30a
safwanpaleri <safwanpaleri@gmail.com>
2021-11-13 13:54:04 +0530

    Update AttackerSpwanerScript.cs

--- AttackerSpwanerScript.cs
+++ AttackerSpwanerScript.cs
@@ -1,43 +1,56 @@
-﻿using System.Collections;
+//Headers.
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class AttackerSpwanerScript : MonoBehaviour
 {
+    //Variables.
     [Header("SPAWNING")]
     bool spwan = true;
     [SerializeField] float minDelay = 1f;
     [SerializeField] float maxDelay = 5f;
 
     [Header("Attacker")]
-    [SerializeField] AttackerScript[] LizardAttacker;
+    //There are different types of attackers.
+    //all the types are saved in a single array.
+    [SerializeField] AttackerScript[] Attacker;
+    
     // Start is called before the first frame update
     IEnumerator Start()
     {
+        //While the spawn still continues.
         if (spwan)
-        {
+        {  
+            //Spawn the enemy after a delay.
             yield return new WaitForSeconds(Random.Range(minDelay, maxDelay));
             spawnAttacker();
         }
     }
 
+    //Function to stop the spwan
     public void StopSpawn()
     {
         spwan = false;
        
     }
+    
+    //Function responsible for spawning the attacker.
     private void spawnAttacker()
     {
-            int index = Random.Range(0, LizardAttacker.Length);
-            spawnindex(LizardAttacker[index]);
+        //Select a random attacker from the array of attackers.
+        int index = Random.Range(0, Attacker.Length);
+        spawnindex(LizardAttacker[index]);
     }
 
+    //Code for spawning the attacker.
     private void spawnindex(AttackerScript newAttacker)
     {
         AttackerScript newAttcker = Instantiate (newAttacker, transform.position, Quaternion.identity) as AttackerScript;
         newAttcker.transform.parent = transform;
     }
 
+    //Change the timing between the spwans according to difficulty.
     public void updatedelay(float diff)
     {
         if(diff == 0 )
